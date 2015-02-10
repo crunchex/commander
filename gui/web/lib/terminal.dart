2eda2c1e84047fc88df68f5c936b7ad05fb9c871
Mike Lewis <mike@updroid.com>
2015-02-09 21:03:38 -0800

    terminal: added row x height calculation

--- gui/web/lib/terminal.dart
+++ gui/web/lib/terminal.dart
@@ -1,11 +1,19 @@
 library terminal;
 
+import 'dart:html';
+import 'dart:async';
+
 class Terminal {
-  int cols, rows;
+  DivElement div;
+  Stream stdin, stdout;
+  int charWidth, charHeight;
   List cursorXY;
   
-  Terminal () {
-    cols = 80;
-    rows = 25;
+  Terminal (this.div, this.stdin, this.stdout) {
+    charWidth = 10;
+    charHeight = 13;
   }
+  
+  int get cols => div.borderEdge.width ~/ charWidth;
+  int get rows => div.borderEdge.height ~/ charHeight;
 }

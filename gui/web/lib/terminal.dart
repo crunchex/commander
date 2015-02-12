e41056c64cdc3ffabf3a8cf27701cd8d00e1d241
Mike Lewis <mike@updroid.com>
2015-02-12 11:36:19 -0800

    terminal: more WIP

--- gui/web/lib/terminal.dart
+++ gui/web/lib/terminal.dart
@@ -7,6 +7,14 @@ import 'console_helper.dart';
 
 const bool debug = false;
 
+/// A class for keeping track of inputHandling state.
+/// Stand-in for what should be an enum.
+class InputMode {
+  static int dump = 0;
+  static int normal = 1;
+  static int escape = 2;
+}
+
 /// A class for rendering a terminal emulator in a [DivElement] (param).
 /// [stdout] needs to receive individual UTF8 integers and will handle
 /// them appropriately.
@@ -15,12 +23,18 @@ class Terminal {
   int _charWidth, _charHeight;
   List _cursorXY;
   
-  List _escapeCode;
-  List _outString;
+  // This should really be an enum.
+  int _inputSwitch;
+  
+  List<int> _escapeCode;
+  List<int> _outString;
   
   StreamController stdout;
+  StreamController _escapeCodes;
+  StreamController _normalStrings;
   
   Terminal (DivElement div) {
+    _inputSwitch = InputMode.normal;
     _charWidth = 10;
     _charHeight = 17;
     _cursorXY = [0, 0];
@@ -29,7 +43,9 @@ class Terminal {
     _escapeCode = [];
     _outString = [];
 
-    stdout = new StreamController<List<int>>();
+    stdout = new StreamController<String>();
+    _escapeCodes = new StreamController<String>();
+    _normalStrings = new StreamController<String>();
     
     registerEventHandlers();
     initDisplay();
@@ -39,7 +55,17 @@ class Terminal {
   int get _rows => div.borderEdge.height ~/ _charHeight - 1;
   
   void registerEventHandlers() {
-    stdout.stream.listen((output) => (debug) ? print(output) : handleOutput(output));
+    stdout.stream.listen((out) {
+      if (debug) {
+        print(out);
+        return;
+      }
+      
+      if (out == '27' || _inputSwitch == InputMode.escape) {
+        _escapeCodes.add(out);
+        
+      }
+    });
   }
   
   initDisplay() {
@@ -59,20 +85,27 @@ class Terminal {
   /// Takes the raw stdout from shell per individual UTF8 int (as a string)
   /// and and handles it appropriately.
   void handleOutput(String output) {
-    // Append if NL or start of Escape Sequence is undetected.
-    if (output != '10' && output != '27') {
-      _outString.add(int.parse(output));
-      return;
+    switch (output) {
+      case '27':
+        // Escape Sequence detected.
+        _escapeCode.add(int.parse(output));
+        break;
+      case '10':
+        // New Line detected.
+        
     }
     
     // Do something with the string before resetting it.
-    print(UTF8.decode(_outString));
+    String str = UTF8.decode(_outString);
+    print('str ' + str);
+    drawString(str);
     _outString = [];
   }
   
   void drawString(String str) {
     print('cursor x before: ' + _cursorXY[0]);
     SpanElement strSpan = new SpanElement();
+    strSpan.innerHtml = str;
     div.children[_cursorXY[0]].children.insert(0, strSpan);
     _cursorXY[0]++;
     print('cursor x after: ' + _cursorXY[0]);

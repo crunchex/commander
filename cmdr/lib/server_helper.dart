e5f779eff5167177b4ac723d266b19be2b71fa26
Mike Lewis <mike@updroid.com>
2015-02-12 18:45:55 -0800

    cmdr: removed print - help.debug() only

--- cmdr/lib/server_helper.dart
+++ cmdr/lib/server_helper.dart
@@ -44,7 +44,6 @@ void debug(String logstring, int level) {
 void formattedMessage(WebSocket socket, String header, String body) {
   socket.add('[[$header]]$body');
   String msg = '[[$header]]$body';
-  print(msg);
 }
 
 /// Helper method to grab file name in case of spaces.

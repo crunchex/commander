7739fe145dd08cbfbd6d805da0702b941997aaf7
Mike Lewis <mike@updroid.com>
2015-03-01 13:38:09 -0800

    terminal: moved terminal-related files into sub directory

--- gui/web/client.dart
+++ gui/web/client.dart
@@ -8,7 +8,7 @@ import 'package:bootjack/bootjack.dart';
 import 'package:ace/proxy.dart';
 import 'package:dnd/dnd.dart';
 import 'lib/updroid_message.dart';
-import 'lib/terminal.dart';
+import 'lib/terminal/terminal.dart';
 
 part 'explorer.dart';
 part 'editor.dart';

80c59ec68bdcf588f32bfca8a31c1ea2b4c14a59
Mike Lewis <mike@updroid.com>
2015-03-09 16:29:15 -0700

    pty: added dynamic enumeration for ptys

--- cmdr/pty.dart
+++ cmdr/pty.dart
@@ -1,18 +1,17 @@
 part of updroid_server;
 
 class UpDroidPty {
-  int _ptyNum;
+  int ptyNum = 1;
 
-  UpDroidPty(String workspacePath) {
+  UpDroidPty(this.ptyNum, String workspacePath) {
     // TODO: this should be dynamically assigned when
     // multiple consoles are spawned.
-    _ptyNum = 0;
 
     // Process launches 'cmdr-pty', a go program that provides a direct hook to a system pty.
     // See http://bitbucket.org/crunchex/cmdr-pty
-    Process.start('cmdr-pty', [], workingDirectory: workspacePath).then((Process shell) {
-      shell.stdout.listen((data) => help.debug('pty[$_ptyNum] stdout: ${UTF8.decode(data)}', 0));
-      shell.stderr.listen((data) => help.debug('pty[$_ptyNum] stderr: ${UTF8.decode(data)}', 0));
+    Process.start('cmdr-pty', ['-addr', ':1206$ptyNum'], workingDirectory: workspacePath).then((Process shell) {
+      shell.stdout.listen((data) => help.debug('pty[$ptyNum] stdout: ${UTF8.decode(data)}', 0));
+      shell.stderr.listen((data) => help.debug('pty[$ptyNum] stderr: ${UTF8.decode(data)}', 0));
     });
   }
 }

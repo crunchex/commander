13c2cf9df0cf9db5610d4a44a57dc89968732f69
Mike Lewis <mike@updroid.com>
2015-03-10 08:11:37 -0700

    console: privatized members

--- gui/web/console.dart
+++ gui/web/console.dart
@@ -13,84 +13,84 @@ Programs that are curses-based, such as htop, will not work.
 /// user to pass in white-listed commands to the server and view their output.
 /// It is not meant to be a complete terminal emulator like xterm.
 class UpDroidConsole {
-  WebSocket ws;
-  StreamController<CommanderMessage> cs;
-  Terminal term;
+  WebSocket _ws;
+  StreamController<CommanderMessage> _cs;
+  Terminal _term;
 
-  DivElement console;
-  AnchorElement consoleButton;
-  AnchorElement themeButton;
-  bool lightTheme;
+  DivElement _console;
+  AnchorElement _consoleButton;
+  AnchorElement _themeButton;
+  bool _lightTheme;
 
   UpDroidConsole(int consoleNum, StreamController<CommanderMessage> cs) {
-    this.cs = cs;
+    _cs = cs;
 
-    console = querySelector('#console-$consoleNum');
-    print(console.id.toString());
-    consoleButton = querySelector('#button-console-$consoleNum');
-    themeButton = querySelector('#button-console-theme-$consoleNum');
+    _console = querySelector('#console-$consoleNum');
+    print(_console.id.toString());
+    _consoleButton = querySelector('#button-console-$consoleNum');
+    _themeButton = querySelector('#button-console-theme-$consoleNum');
 
-    term = new Terminal(console);
-    term
+    _term = new Terminal(_console);
+    _term
         ..scrollSpeed = 3
         ..theme = 'solarized-dark';
 
-    lightTheme = false;
+    _lightTheme = false;
 
-    initWebSocket('ws://localhost:1206$consoleNum/pty');
-    registerConsoleEventHandlers();
+    _initWebSocket('ws://localhost:1206$consoleNum/pty');
+    _registerConsoleEventHandlers();
 
     cs.add(new CommanderMessage('CLIENT', 'CONSOLE_READY'));
   }
 
   /// Toggles between a Solarized dark and light theme.
-  void toggleTheme() {
-    if (lightTheme) {
-      term.theme = 'solarized-dark';
-      lightTheme = false;
+  void _toggleTheme() {
+    if (_lightTheme) {
+      _term.theme = 'solarized-dark';
+      _lightTheme = false;
     } else {
-      term.theme = 'solarized-light';
-      lightTheme = true;
+      _term.theme = 'solarized-light';
+      _lightTheme = true;
     }
   }
 
   /// Sets up the event handlers for the console.
-  void registerConsoleEventHandlers() {
-    ws.onMessage.listen((e) {
+  void _registerConsoleEventHandlers() {
+    _ws.onMessage.listen((e) {
       ByteBuffer buf = e.data;
-      term.stdout.add(buf.asUint8List());
+      _term.stdout.add(buf.asUint8List());
     });
 
-    term.stdin.stream.listen((data) {
-      ws.sendByteBuffer(new Uint8List.fromList(data).buffer);
+    _term.stdin.stream.listen((data) {
+      _ws.sendByteBuffer(new Uint8List.fromList(data).buffer);
     });
 
-    themeButton.onClick.listen((e) {
-      toggleTheme();
+    _themeButton.onClick.listen((e) {
+      _toggleTheme();
       e.preventDefault();
     });
   }
 
-  void initWebSocket(String url, [int retrySeconds = 2]) {
+  void _initWebSocket(String url, [int retrySeconds = 2]) {
     bool encounteredError = false;
 
-    ws = new WebSocket(url);
-    ws.binaryType = "arraybuffer";
+    _ws = new WebSocket(url);
+    _ws.binaryType = "arraybuffer";
 
-    ws.onOpen.listen((e) => print('Console connected.'));
+    _ws.onOpen.listen((e) => print('Console connected.'));
 
-    ws.onClose.listen((e) {
+    _ws.onClose.listen((e) {
       print('Console disconnected. Retrying...');
       if (!encounteredError) {
-        new Timer(new Duration(seconds:retrySeconds), () => initWebSocket(url, retrySeconds * 2));
+        new Timer(new Duration(seconds:retrySeconds), () => _initWebSocket(url, retrySeconds * 2));
       }
       encounteredError = true;
     });
 
-    ws.onError.listen((e) {
+    _ws.onError.listen((e) {
       print('Console disconnected. Retrying...');
       if (!encounteredError) {
-        new Timer(new Duration(seconds:retrySeconds), () => initWebSocket(url, retrySeconds * 2));
+        new Timer(new Duration(seconds:retrySeconds), () => _initWebSocket(url, retrySeconds * 2));
       }
       encounteredError = true;
     });

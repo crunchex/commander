8c8ec16d17edcad522a1aa896ea617f3fb7f52aa
Mike Lewis <mike@updroid.com>
2015-03-10 11:33:15 -0700

    tools: split the build from the packaging script

--- tools/make_pkg.sh
+++ tools/make_pkg.sh
@@ -13,36 +13,8 @@ echo "OK"
 
 cd $DIR/../
 
-### cmdr ###
-cd cmdr
-
-echo -n "Getting dependencies for cmdr..."
-pub get > /dev/null
-echo "OK"
-
-echo -n "Building (minifying) cmdr......."
-mkdir -p bin
-dart2js --output-type=dart --categories=Server --minify -o cmdr cmdr.dart > /dev/null
-rm cmdr.deps
-sed -i '1i#!/usr/bin/env dart' cmdr
-chmod +x cmdr
-mv cmdr bin/
-echo "OK"
-
-cd ../
-
-### gui ###
-cd gui
-
-echo -n "Getting dependencies for gui...."
-pub get > /dev/null
-echo "OK"
-
-echo -n "Building (minifying) gui........"
-pub build > /dev/null
-echo "OK"
-
-cd ../
+### build ###
+tools/build_cmdr.sh
 
 ### package ###
 echo -n "Packaging......................."

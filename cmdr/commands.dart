a7920ee7dccbdfd84faf79103bced9c9fd36c9d3
Mike Lewis <mike@updroid.com>
2015-04-25 13:13:11 -0700

    commands: updated command help description

--- cmdr/commands.dart
+++ cmdr/commands.dart
@@ -5,9 +5,12 @@ class CmdrGuiCommand extends Command {
   final description = "Launch UpDroid Commander in your browser.";
 
   CmdrGuiCommand() {
-    argParser.addFlag('debug', abbr: 'd', defaultsTo: CmdrServer.defaultDebugFlag, help: 'Prints debug messages to server.log');
-    argParser.addOption('workspace', abbr: 'w', defaultsTo: CmdrServer.defaultWorkspacePath, help: 'Overrides the default workspace for Commander.');
-    argParser.addOption('path', abbr: 'p', defaultsTo: CmdrServer.defaultGuiPath, help: 'Overrides the default path of the gui files location.');
+    argParser.addFlag('debug', abbr: 'd', defaultsTo: CmdrServer.defaultDebugFlag,
+        help: 'Prints debug messages to cmdr.log');
+    argParser.addOption('workspace', abbr: 'w', defaultsTo: CmdrServer.defaultWorkspacePath,
+        help: 'Overrides the default workspace for cmdr.');
+    argParser.addOption('path', abbr: 'p', defaultsTo: CmdrServer.defaultGuiPath,
+        help: 'Overrides the default path of the gui files location.\nOnly useful for cmdr developers.');
   }
 
   void run() {

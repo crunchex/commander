9f4c0d499f688f82d4277de8957ff3d63b791e98
Mike Lewis <mike@updroid.com>
2015-05-02 13:10:49 -0700

    gitignore: added bin dir

---
+++ bin/cmdr.dart
@@ -0,0 +1,16 @@
+#!/usr/bin/env dart
+
+import 'package:args/command_runner.dart';
+
+import '../lib/server.dart';
+
+void main(List<String> args) {
+  CommandRunner runner = new CommandRunner("cmdr", "The UpDroid Command tool.")
+    ..addCommand(new CmdrGuiCommand());
+
+    runner.run(args).catchError((error) {
+      if (error is! UsageException) throw error;
+      print(error);
+      return;
+    });
+}

42e9ff729ddd7a8649a773375471094035f795cb
Mike Lewis <mike@updroid.com>
2015-05-05 14:02:06 -0700

    client: now uses the mailbox class

--- web/lib/client.dart
+++ web/lib/client.dart
@@ -11,12 +11,13 @@ import 'tabs/console.dart';
 import 'tabs/camera/camera.dart';
 import 'modal/modal.dart';
 import 'updroid_message.dart';
+import 'mailbox.dart';
 
 class UpDroidClient {
-  WebSocket ws;
-  StreamController<CommanderMessage> cs;
+  StreamController<CommanderMessage> _cs;
 
   List<List> _tabs;
+  String _config;
 
   AnchorElement _newButtonLeft;
   AnchorElement _newButtonRight;
@@ -24,6 +25,8 @@ class UpDroidClient {
   ButtonElement _buildButton;
   ButtonElement _runButton;
   ButtonElement _uploadButton;
+
+  Mailbox _mailbox;
 
   String status;
   bool encounteredError;
@@ -35,8 +38,7 @@ class UpDroidClient {
   ElementStream chooseVideo;
 
   UpDroidClient() {
-    this.status = 'DISCONNECTED';
-    this.encounteredError = false;
+    _config = _getConfig();
 
     _tabs = [[], [], []];
 
@@ -49,19 +51,13 @@ class UpDroidClient {
 
     _runButtonEnabled = true;
 
-    String config = _getConfig();
-
     // Create the intra-client message stream.
     // The classes use this to communicate with each other.
-    this.cs = new StreamController<CommanderMessage>.broadcast();
+    _cs = new StreamController<CommanderMessage>.broadcast();
+    _mailbox = new Mailbox('updroidclient', 1, _cs);
 
-    // Create the server <-> client [WebSocket].
-    // Port 12060 is the default port that UpDroid uses.
-    String url = window.location.host;
-    url = url.split(':')[0];
-    initWebSocket('ws://' + url + ':12060/server/1');
-
-    registerEventHandlers(ws, cs, config);
+    _registerMailbox();
+    registerEventHandlers(_getConfig());
 
     AnchorElement feedbackButton = querySelector('#feedback-button');
     pulseFeedback(feedbackButton);
@@ -82,136 +78,6 @@ class UpDroidClient {
       new Timer(new Duration(milliseconds: 500), () {
         feedbackButton.classes.remove('feedback-bold');
       });
-    });
-  }
-
-  /// Process messages according to the type.
-  void processMessage(CommanderMessage m) {
-    switch (m.type) {
-      case 'CLOSE_TAB':
-        _closeTab(m.body);
-        break;
-
-      case 'OPEN_TAB':
-        List idList = m.body.split('_');
-        int column = int.parse(idList[0]);
-        String className = idList[1];
-
-        int id = _getAvailableId(className);
-        _openTab(column, id, className);
-        break;
-
-      case 'GIT_PASSWORD':
-        // TODO: need a more reliable handle to Explorer.
-        ws.send('[[GIT_PUSH]]' + '${_tabs[0].first.currentSelectedPath}++${m.body}');
-        break;
-
-      case 'WORKSPACE_CLEAN':
-        _cleanButton.children.first.classes.removeAll(['glyphicons-refresh', 'glyph-progress']);
-        _cleanButton.children.first.classes.add('glyphicons-cleaning');
-        break;
-
-      case 'WORKSPACE_BUILD':
-        // Success else failure.
-        if (m.body == '') {
-          _runButton.classes.remove('control-button-disabled');
-          _runButtonEnabled = true;
-        } else {
-          new UpDroidBuildResultsModal(m.body);
-        }
-
-        _buildButton.children.first.classes.removeAll(['glyphicons-refresh', 'glyph-progress']);
-        _buildButton.children.first.classes.add('glyphicons-classic-hammer');
-        break;
-
-      default:
-        print('Client warning: received unrecognized message type ${m.type}');
-    }
-  }
-
-  void initWebSocket(String url, [int retrySeconds = 2]) {
-    bool encounteredError = false;
-
-    ws = new WebSocket(url);
-
-    ws.onOpen.listen((e) {
-      status = 'CONNECTED';
-      ws.send('[[CLIENT_CONFIG]]');
-      cs.add(new CommanderMessage('ALL', status));
-    });
-
-    ws.onClose.listen((e) {
-      status = 'DISCONNECTED';
-      cs.add(new CommanderMessage('ALL', status));
-      if (!encounteredError) {
-        new Timer(new Duration(seconds: retrySeconds), () => initWebSocket(url, retrySeconds * 2));
-      }
-      encounteredError = true;
-    });
-
-    ws.onError.listen((e) {
-      if (!encounteredError) {
-        new Timer(new Duration(seconds: retrySeconds), () => initWebSocket(url, retrySeconds * 2));
-      }
-      encounteredError = true;
-    });
-  }
-
-  /// Sets up external event handlers for the various Commander classes. These
-  /// are mostly listening events for [WebSocket] messages.
-  void registerEventHandlers(WebSocket ws, StreamController<CommanderMessage> cs, String config) {
-    cs.stream.where((m) => m.dest == 'CLIENT').listen((m) => processMessage(m));
-
-    ws.onMessage
-        .transform(updroidTransformer)
-        .where((um) => um.header == 'CLIENT_SERVER_READY')
-        .listen((um) => _initializeTabs(config, JSON.decode(um.body)));
-
-    ws.onMessage
-        .transform(updroidTransformer)
-        .where((um) => um.header == 'CATKIN_NODE_LIST')
-        .listen((um) {
-      new UpDroidRunNodeModal(JSON.decode(um.body), ws);
-    });
-
-    _newButtonLeft.onClick.listen((e) {
-      e.preventDefault();
-      if (_tabs[1].length >= 4) return;
-
-      new UpDroidOpenTabModal(1, cs);
-    });
-
-    _newButtonRight.onClick.listen((e) {
-      e.preventDefault();
-      if (_tabs[2].length >= 4) return;
-
-      new UpDroidOpenTabModal(2, cs);
-    });
-
-    _cleanButton.onClick.listen((e) {
-      _cleanButton.children.first.classes.remove('glyphicons-cleaning');
-      _cleanButton.children.first.classes.addAll(['glyphicons-refresh', 'glyph-progress']);
-
-      cs.add(new CommanderMessage('EXPLORER', 'WORKSPACE_CLEAN'));
-
-      _runButton.classes.add('control-button-disabled');
-      _runButtonEnabled = false;
-    });
-
-    _buildButton.onClick.listen((e) {
-      _buildButton.children.first.classes.remove('glyphicons-classic-hammer');
-      _buildButton.children.first.classes.addAll(['glyphicons-refresh', 'glyph-progress']);
-
-      cs.add(new CommanderMessage('EXPLORER', 'WORKSPACE_BUILD'));
-    });
-
-    _runButton.onClick.listen((e) {
-      if (!_runButtonEnabled) return;
-      ws.send('[[CATKIN_NODE_LIST]]');
-    });
-
-    _uploadButton.onClick.listen((e) {
-      new UpDroidGitPassModal(cs);
     });
   }
 
@@ -277,7 +143,7 @@ class UpDroidClient {
         explorer.hideExplorer();
       }
     }
-    _tabs[0].add(new UpDroidExplorer(cs, id, name));
+    _tabs[0].add(new UpDroidExplorer(_cs, id, name));
   }
 
   Future _openTab (int column, int id, String className) async {
@@ -288,20 +154,23 @@ class UpDroidClient {
     }
 
     if (className == 'UpDroidEditor') {
-      _tabs[column].add(new UpDroidEditor(id, column, cs, active: true));
-      ws.send('[[OPEN_TAB]]' + '$column-$id-$className');
+      _tabs[column].add(new UpDroidEditor(id, column, _cs, active: true));
+      _mailbox.ws.send('[[OPEN_TAB]]' + '$column-$id-$className');
     } else if (className == 'UpDroidCamera') {
-      _tabs[column].add(new UpDroidCamera(id, column, cs, active: true));
-      ws.send('[[OPEN_TAB]]' + '$column-$id-$className');
+      _tabs[column].add(new UpDroidCamera(id, column, _cs, active: true));
+      _mailbox.ws.send('[[OPEN_TAB]]' + '$column-$id-$className');
     } else if (className == 'UpDroidConsole') {
       //Isolate console = await spawnDomUri(new Uri.file('lib/tabs/console.dart'), ['test'], [id, column, true]);
-      _tabs[column].add(new UpDroidConsole(id, column, cs, active: true));
+      _tabs[column].add(new UpDroidConsole(id, column, _cs, active: true));
       // TODO: initial size should not be hardcoded.
-      ws.send('[[OPEN_TAB]]' + '$column-$id-$className-25-80');
+      _mailbox.ws.send('[[OPEN_TAB]]' + '$column-$id-$className-25-80');
     }
   }
 
-  void _closeTab(String id) {
+  //\/\/ Mailbox Handlers /\/\//
+
+  void _closeTab(CommanderMessage m) {
+    String id = m.body;
     List idList = id.split('_');
     String type = idList[0];
     int num = int.parse(idList[1]);
@@ -316,6 +185,97 @@ class UpDroidClient {
       }
     }
 
-    ws.send('[[CLOSE_TAB]]' + id);
+    _mailbox.ws.send('[[CLOSE_TAB]]' + id);
+  }
+
+  void _openTabFromButton(CommanderMessage m) {
+    List idList = m.body.split('_');
+    int column = int.parse(idList[0]);
+    String className = idList[1];
+
+    int id = _getAvailableId(className);
+    _openTab(column, id, className);
+  }
+
+  void _gitPassword(CommanderMessage m) {
+    _mailbox.ws.send('[[GIT_PUSH]]' + '${_tabs[0].first.currentSelectedPath}++${m.body}');
+  }
+
+  void _workspaceClean(CommanderMessage m) {
+    _cleanButton.children.first.classes.removeAll(['glyphicons-refresh', 'glyph-progress']);
+    _cleanButton.children.first.classes.add('glyphicons-cleaning');
+  }
+
+  void _workspaceBuild(CommanderMessage m) {
+    // Success else failure.
+    if (m.body == '') {
+      _runButton.classes.remove('control-button-disabled');
+      _runButtonEnabled = true;
+    } else {
+      new UpDroidBuildResultsModal(m.body);
+    }
+
+    _buildButton.children.first.classes.removeAll(['glyphicons-refresh', 'glyph-progress']);
+    _buildButton.children.first.classes.add('glyphicons-classic-hammer');
+  }
+
+  void _sendClientConfig(UpDroidMessage um) => _mailbox.ws.send('[[CLIENT_CONFIG]]');
+  void _serverReady(UpDroidMessage um) => _initializeTabs(_config, JSON.decode(um.body));
+  void _nodeList(UpDroidMessage um) { new UpDroidRunNodeModal(JSON.decode(um.body), _mailbox.ws); }
+
+  void _registerMailbox() {
+    _mailbox.registerCommanderEvent('CLOSE_TAB', _closeTab);
+    _mailbox.registerCommanderEvent('OPEN_TAB', _openTabFromButton);
+    _mailbox.registerCommanderEvent('GIT_PASSWORD', _gitPassword);
+    _mailbox.registerCommanderEvent('WORKSPACE_CLEAN', _workspaceClean);
+    _mailbox.registerCommanderEvent('WORKSPACE_BUILD', _workspaceBuild);
+
+    _mailbox.registerWebSocketEvent(EventType.ON_OPEN, 'CLIENT_CONFIG', _sendClientConfig);
+    _mailbox.registerWebSocketEvent(EventType.ON_MESSAGE, 'CLIENT_SERVER_READY', _serverReady);
+    _mailbox.registerWebSocketEvent(EventType.ON_MESSAGE, 'CATKIN_NODE_LIST', _nodeList);
+  }
+
+  /// Sets up external event handlers for the various Commander classes. These
+  /// are mostly listening events for [WebSocket] messages.
+  void registerEventHandlers(String config) {
+    _newButtonLeft.onClick.listen((e) {
+      e.preventDefault();
+      if (_tabs[1].length >= 4) return;
+
+      new UpDroidOpenTabModal(1, _cs);
+    });
+
+    _newButtonRight.onClick.listen((e) {
+      e.preventDefault();
+      if (_tabs[2].length >= 4) return;
+
+      new UpDroidOpenTabModal(2, _cs);
+    });
+
+    _cleanButton.onClick.listen((e) {
+      _cleanButton.children.first.classes.remove('glyphicons-cleaning');
+      _cleanButton.children.first.classes.addAll(['glyphicons-refresh', 'glyph-progress']);
+
+      _cs.add(new CommanderMessage('EXPLORER', 'WORKSPACE_CLEAN'));
+
+      _runButton.classes.add('control-button-disabled');
+      _runButtonEnabled = false;
+    });
+
+    _buildButton.onClick.listen((e) {
+      _buildButton.children.first.classes.remove('glyphicons-classic-hammer');
+      _buildButton.children.first.classes.addAll(['glyphicons-refresh', 'glyph-progress']);
+
+      _cs.add(new CommanderMessage('EXPLORER', 'WORKSPACE_BUILD'));
+    });
+
+    _runButton.onClick.listen((e) {
+      if (!_runButtonEnabled) return;
+      _mailbox.ws.send('[[CATKIN_NODE_LIST]]');
+    });
+
+    _uploadButton.onClick.listen((e) {
+      new UpDroidGitPassModal(_cs);
+    });
   }
 }

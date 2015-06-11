82c2557f1eb5e501136e96eb87a8e266bf5ed2a8
Mike Lewis <mike@updroid.com>
2015-06-11 09:50:02 -0700

    server: added debug line

--- lib/server.dart
+++ lib/server.dart
@@ -56,6 +56,8 @@ class CmdrServer {
     virDir = new VirtualDirectory(Platform.script.resolve(guiPath).toFilePath())
         ..allowDirectoryListing = true
         ..followLinks = true
+        // Uncomment to serve to Dartium for debugging.
+        //..jailroot = false
         ..directoryHandler = (dir, request) {
           // Redirects '/' to 'index.html'
           var indexUri = new Uri.file(dir.path).resolve('index.html');

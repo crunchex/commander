dedbe8e8f6d6f42f6c6e279cdcf777e09ed75055
Mike Lewis <mike@updroid.com>
2015-06-22 15:40:11 -0700

    nodes: moved ros entity classes into new file

--- web/lib/panels/explorer/nodes/nodes_controller.dart
+++ web/lib/panels/explorer/nodes/nodes_controller.dart
@@ -10,6 +10,7 @@ import '../../panel_controller.dart';
 import '../explorer.dart';
 
 part 'nodes_view.dart';
+part 'ros_entity.dart';
 
 class UpDroidNodes implements ExplorerController {
   PanelView _view;
@@ -65,106 +66,5 @@ class UpDroidNodes implements ExplorerController {
     packages.values.forEach((Package p) => p.cleanUp());
     packages = null;
     _nodesView.cleanUp();
-  }
-}
-
-class Package {
-  String name, path;
-  List<Node> nodes;
-  PackageView view;
-
-  Package(this.name, this.path) {
-    nodes = [];
-
-    setUpPackageView();
-  }
-
-  void setUpPackageView() {
-    view = new PackageView(name);
-
-    view.container.onDoubleClick.listen((e) {
-      view.toggleExpansion();
-    });
-  }
-
-  void cleanUp() {
-    nodes.forEach((Node n) => n.cleanUp());
-    view.cleanUp();
-  }
-}
-
-class Node {
-  String name, packageName;
-  List args;
-  NodeView view;
-
-  WebSocket _ws;
-
-  bool _selectEnabled, _selected;
-
-  Node(this.name, this.args, this.packageName, WebSocket ws) {
-    _ws = ws;
-    _selected = false;
-    _selectEnabled = true;
-
-    _setUpNodeView();
-
-    //print('workspacePath: $workspacePath, path: $path, name: $name, parent: $parent');
-  }
-
-  void runNode() {
-    List runCommand = [];
-    runCommand.addAll([packageName, name]);
-    view.uElement.children.forEach((LIElement li) {
-      List<String> arg = [];
-
-      DivElement container = li.firstChild;
-      arg.add(container.firstChild.text);
-      arg.add(container.lastChild.value);
-
-      runCommand.add(arg);
-    });
-
-    _ws.send('[[RUN_NODE]]' + JSON.encode(runCommand));
-  }
-
-
-  void _setUpNodeView() {
-    view = new NodeView(name, args);
-
-    view.container.onClick.listen((e) {
-      if (_selectEnabled) {
-        toggleSelected();
-        _selectEnabled = false;
-
-        new Timer(new Duration(milliseconds: 500), () {
-          _selectEnabled = true;
-        });
-      }
-    });
-
-    view.container.onContextMenu.listen((e) {
-      e.preventDefault();
-      List menu = [
-        {'type': 'toggle', 'title': 'Run', 'handler': runNode}];
-      ContextMenu.createContextMenu(e.page, menu);
-    });
-  }
-
-  void toggleSelected() => _selected ? deselect() : select();
-
-  void select() {
-    view.select();
-    _selected = true;
-  }
-
-  void deselect() {
-    view.deselect();
-    _selected = false;
-  }
-
-  void cleanUp() {
-    //_contextListeners.forEach((StreamSubscription listener) => listener.cancel());
-    view.cleanUp();
   }
 }
---
+++ web/lib/panels/explorer/nodes/ros_entity.dart
@@ -0,0 +1,102 @@
+part of updroid_explorer_nodes;
+
+class Package {
+  String name, path;
+  List<Node> nodes;
+  PackageView view;
+
+  Package(this.name, this.path) {
+    nodes = [];
+
+    setUpPackageView();
+  }
+
+  void setUpPackageView() {
+    view = new PackageView(name);
+
+    view.container.onDoubleClick.listen((e) {
+      view.toggleExpansion();
+    });
+  }
+
+  void cleanUp() {
+    nodes.forEach((Node n) => n.cleanUp());
+    view.cleanUp();
+  }
+}
+
+class Node {
+  String name, packageName;
+  List args;
+  NodeView view;
+
+  WebSocket _ws;
+
+  bool _selectEnabled, _selected;
+
+  Node(this.name, this.args, this.packageName, WebSocket ws) {
+    _ws = ws;
+    _selected = false;
+    _selectEnabled = true;
+
+    _setUpNodeView();
+
+    //print('workspacePath: $workspacePath, path: $path, name: $name, parent: $parent');
+  }
+
+  void runNode() {
+    List runCommand = [];
+    runCommand.addAll([packageName, name]);
+    view.uElement.children.forEach((LIElement li) {
+      List<String> arg = [];
+
+      DivElement container = li.firstChild;
+      arg.add(container.firstChild.text);
+      arg.add(container.lastChild.value);
+
+      runCommand.add(arg);
+    });
+
+    _ws.send('[[RUN_NODE]]' + JSON.encode(runCommand));
+  }
+
+
+  void _setUpNodeView() {
+    view = new NodeView(name, args);
+
+    view.container.onClick.listen((e) {
+      if (_selectEnabled) {
+        toggleSelected();
+        _selectEnabled = false;
+
+        new Timer(new Duration(milliseconds: 500), () {
+          _selectEnabled = true;
+        });
+      }
+    });
+
+    view.container.onContextMenu.listen((e) {
+      e.preventDefault();
+      List menu = [
+        {'type': 'toggle', 'title': 'Run', 'handler': runNode}];
+      ContextMenu.createContextMenu(e.page, menu);
+    });
+  }
+
+  void toggleSelected() => _selected ? deselect() : select();
+
+  void select() {
+    view.select();
+    _selected = true;
+  }
+
+  void deselect() {
+    view.deselect();
+    _selected = false;
+  }
+
+  void cleanUp() {
+    //_contextListeners.forEach((StreamSubscription listener) => listener.cancel());
+    view.cleanUp();
+  }
+}

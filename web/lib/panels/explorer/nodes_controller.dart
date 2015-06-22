dec96b952f370550c04365218b16daedd6d73d1f
Mike Lewis <mike@updroid.com>
2015-06-22 11:48:35 -0700

    explorer: removed dead code

--- web/lib/panels/explorer/nodes_controller.dart
+++ web/lib/panels/explorer/nodes_controller.dart
@@ -51,36 +51,6 @@ class UpDroidNodes implements ExplorerController {
     packages[packageName].view.element.children.add(node.view.element);
   }
 
-  void populateNodes(UpDroidMessage um) {
-    List<Map> nodeList = JSON.decode(um.body);
-    Map packageMap = _nodesView.createPackageList(nodeList);
-
-    for (var packageNode in nodeList) {
-      if(!packageNode['node'].contains('.xml')) {
-        var element = _nodesView.createNodeLi(cs, packageNode);
-        var listToAppend = packageMap[packageNode['package']];
-        listToAppend.append(element);
-        setupNodeHighlighter(element);
-      }
-    }
-  }
-
-  void setupNodeHighlighter (LIElement li) {
-    li.onClick.listen((e) {
-      if(currentSelectedNode != null) {
-        currentSelectedNode.classes.remove('highlighted');
-        nodeArgs.classes.add('hidden');
-      }
-      li.classes.add('highlighted');
-      currentSelectedNode = li;
-      nodeArgs = li.lastChild;
-      runParams.addAll({'name' : li.dataset['name'], 'package' : li.dataset['package'], 'package-path' : li.dataset['package-path']});
-      nodeArgs.classes.remove('hidden');
-    });
-  }
-
-  void _catkinNodeList(CommanderMessage m) => mailbox.ws.send('[[CATKIN_NODE_LIST]]');
-
   void _runNode() {
     String runCommand;
     if (nodeArgs.value.isEmpty) {

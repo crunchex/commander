8e596cc2efd0434e85b691a55df2bb5af3ad0cca
Mike Lewis <mike@updroid.com>
2015-07-09 14:05:29 -0700

    cmdr: changed the help menu program title

--- bin/cmdr.dart
+++ bin/cmdr.dart
@@ -7,7 +7,7 @@ import '../lib/server.dart';
 void main(List<String> args) {
   const String version = '0.4.8 Alpha';
 
-  CommandRunner runner = new CommandRunner("cmdr", "The UpDroid Command tool.")
+  CommandRunner runner = new CommandRunner("cmdr", "UpDroid Commander - Robotics Software IDE.")
     ..addCommand(new InfoCommand(version))
     ..addCommand(new CmdrGuiCommand());
 

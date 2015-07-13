90ff16089e52638ed8dfa84f08fc89dd6a32894f
Mike Lewis <mike@updroid.com>
2015-07-13 13:15:03 -0700

    column: cleaned up view of column content in minimized state

--- web/lib/column_view.dart
+++ web/lib/column_view.dart
@@ -24,7 +24,8 @@ class ColumnView {
   AnchorElement controlButton, maximizeButton;
 
   SpanElement _maximizeGlyph;
-  DivElement _rowMain;
+  UListElement _navTabs;
+  DivElement _rowMain, _tabContent;
 
   ColumnView(this.id, this.state) {
     _rowMain = querySelector('#row-main');
@@ -44,13 +45,13 @@ class ColumnView {
     ..classes.add((state == ColumnState.MAXIMIZED) ? 'glyphicons-resize-small' : 'glyphicons-resize-full');
     maximizeButton.children.add(_maximizeGlyph);
 
-    UListElement navTabs = new UListElement()
+    _navTabs = new UListElement()
     ..classes.addAll(['nav', 'nav-tabs'])
     ..attributes['role'] = 'tablist';
-    columnContent.children.add(navTabs);
+    columnContent.children.add(_navTabs);
 
     LIElement controlLi = new LIElement();
-    navTabs.children.add(controlLi);
+    _navTabs.children.add(controlLi);
 
     controlButton = new AnchorElement( )
     ..id = 'column-$id-new'
@@ -61,10 +62,10 @@ class ColumnView {
     ..classes.addAll(['glyphicons', 'glyphicons-plus']);
     controlButton.children.add(controlGlyph);
 
-    DivElement tabContent = new DivElement()
+    _tabContent = new DivElement()
     ..id = 'col-$id-tab-content'
     ..classes.add('tab-content');
-    columnContent.children.add(tabContent);
+    columnContent.children.add(_tabContent);
   }
 
   void maximize() {
@@ -72,9 +73,10 @@ class ColumnView {
     columnContent.classes.add('col-xs-9');
 
     columnContent.style.width = (querySelector('#column-0').clientWidth <= 200) ? 'calc(100% - 200px)' : '';
-//    columnContent.style.visibility = 'visible';
     _maximizeGlyph.classes.remove('glyphicons-resize-full');
     _maximizeGlyph.classes.add('glyphicons-resize-small');
+
+    _showTabsAndContent();
   }
 
   void normalize() {
@@ -82,9 +84,10 @@ class ColumnView {
     columnContent.classes.add('col-xs-5');
 
     columnContent.style.width = '';
-//    columnContent.style.visibility = 'visible';
     _maximizeGlyph.classes.remove('glyphicons-resize-small');
     _maximizeGlyph.classes.add('glyphicons-resize-full');
+
+    _showTabsAndContent();
   }
 
   void minimize() {
@@ -93,9 +96,26 @@ class ColumnView {
     columnContent.classes.add('col-xs-1');
 
     columnContent.style.width = '';
-//    columnContent.style.visibility = 'hidden';
     _maximizeGlyph.classes.remove('glyphicons-resize-small');
     _maximizeGlyph.classes.add('glyphicons-resize-full');
+
+    _hideTabsAndContent();
+  }
+
+  void _showTabsAndContent() {
+    _navTabs.children.getRange(1, _navTabs.children.length)
+    .forEach((e) => e.children
+    .forEach((e) => e.style.display = ''));
+
+    _tabContent.children.forEach((e) => e.style.display = '');
+  }
+
+  void _hideTabsAndContent() {
+    _navTabs.children.getRange(1, _navTabs.children.length)
+    .forEach((e) => e.children
+    .forEach((e) => e.style.display = 'none'));
+
+    _tabContent.children.forEach((e) => e.style.display = 'none');
   }
 
   void cleanUp() {

6ce1f5f3dbdde653cf44538a536bf7ddfe860adf
Mike Lewis <mike@updroid.com>
2015-07-15 16:13:09 -0700

    api: added support for endpoint registration to tab mailbox

--- lib/tab/api/tab_mailbox.dart
+++ lib/tab/api/tab_mailbox.dart
@@ -1,5 +1,6 @@
 library tab_mailbox;
 
+import 'dart:io';
 import 'dart:isolate';
 
 import 'updroid_message.dart';
@@ -7,11 +8,13 @@ import 'updroid_message.dart';
 /// Manages message passing for a tab.
 class TabMailbox {
   Map _registry;
+  Map _endpointRegistry;
   ReceivePort _receivePort;
   SendPort _sendPort;
 
   TabMailbox(SendPort replyTo) {
     _registry = {};
+    _endpointRegistry = {};
 
     _receivePort.transform(Msg.toMsg).listen((Msg m) {
       print('Received ${m.header}: "${m.body}"');
@@ -19,12 +22,18 @@ class TabMailbox {
     });
   }
 
+  /// Sends out a [Msg] through the [SendPort] associated with this [TabMailbox].
+  void send(Msg m) => _sendPort.send(m.toString());
+
   /// Registers a [function] to be called when the Port receives a message that matches
   /// its associated header key.
   void registerMessageHandler(String header, function(String s)) {
     _registry[header] = function;
   }
 
-  /// Sends out a [Msg] through the [SendPort] associated with this [TabMailbox].
-  void send(Msg m) => _sendPort.send(m.toString());
+  /// Registers a [function] to be called when a request is received at
+  /// a given endpoint (excluding the main one used by [registerWebSocketEvent]).
+  void registerEndpointHandler(String uri, function(HttpRequest request)) {
+    _endpointRegistry[uri] = function;
+  }
 }

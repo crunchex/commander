0cf7a72aa08a97c45dc0476e9b6df6cac0d1a94d
Mike Lewis <mike@updroid.com>
2015-07-15 16:12:22 -0700

    api: added methods to match up server message style with msg

--- lib/tab/api/server_message.dart
+++ lib/tab/api/server_message.dart
@@ -1,5 +1,7 @@
 library server_message;
 
+import 'dart:async';
+
 import 'updroid_message.dart';
 
 class ServerMessage {
@@ -12,4 +14,28 @@ class ServerMessage {
   /// registered id number, 0 for all destinations of type [receiverClass], or
   /// any positive integer for one specific destination.
   ServerMessage(this.receiverClass, this.id, this.um);
+
+  ServerMessage.fromString(String raw) {
+    int endIndex = raw.indexOf(':/s');
+    String serverHeader = raw.substring(2, endIndex);
+
+    List<String> split = serverHeader.split(':');
+    receiverClass = split[0];
+    id = int.parse(split[1]);
+    um = raw.substring(endIndex + 3, raw.length);
+  }
+
+  String toString() {
+    return 's:$receiverClass:$id:/s:${um.toString()}';
+  }
+
+  /// Transformer to convert String messages into the ServerMessages.
+  static StreamTransformer toServerMessage = new StreamTransformer.fromHandlers(handleData: (event, sink) {
+    sink.add(new Msg.fromString(event.data));
+  });
+
+  /// Transformer to convert ServerMessages into Strings that could be sent over Websockets or ports.
+  static StreamTransformer fromServerMessage = new StreamTransformer.fromHandlers(handleData: (event, sink) {
+    sink.add(event.data.s);
+  });
 }

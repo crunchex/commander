c161a64878bfa7ff904e7d5ce76c1123638a982f
Mike Lewis <mike@updroid.com>
2015-07-23 17:27:22 -0700

    tool: removed cmdr-pty build stuff to console repo

--- tool/make_pkg.sh
+++ tool/make_pkg.sh
@@ -27,7 +27,6 @@ done
 ### build ###
 if [ $nobuild == 0 ]; then
     $TOPDIR/tool/build_cmdr.sh
-    $TOPDIR/tool/build_cmdr_pty.sh
 fi
 
 ### start packaging ###
@@ -63,8 +62,8 @@ fpm -s dir -t deb -n cmdr -v 0.5.1 -p $TOPDIR/deploy/ \
     --before-install=$TOPDIR/tool/packaging/before-install.sh \
     --after-install=$TOPDIR/tool/packaging/after-install.sh \
     ./build/web=/opt/updroid/cmdr \
-    ./bin/cmdr=/usr/local/bin/cmdr \
-    $GOPATH/bin/cmdr-pty=/usr/local/bin/cmdr-pty #> /dev/null
+    ./bin/cmdr=/usr/local/bin/cmdr
+    #$GOPATH/bin/cmdr-pty=/usr/local/bin/cmdr-pty #> /dev/null
 
 echo "OK"
 

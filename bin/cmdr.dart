fbefd30eefd7ee9564bbd668de9830f62c5a4e05
Mike Lewis <mike@updroid.com>
2015-07-30 11:22:48 -0700

    cmdr: version bump to 0.5.2

--- bin/cmdr.dart
+++ bin/cmdr.dart
@@ -5,7 +5,7 @@ import 'package:args/command_runner.dart';
 import '../lib/server.dart';
 
 void main(List<String> args) {
-  const String version = '0.5.1 Alpha';
+  const String version = '0.5.2 Alpha';
 
   CommandRunner runner = new CommandRunner("cmdr", "UpDroid Commander - Robotics Software IDE.")
     ..addCommand(new InfoCommand(version))

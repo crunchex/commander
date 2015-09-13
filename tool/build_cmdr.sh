38968fa1c59913609b37063b8db63c8342b30c0b
Mike Lewis <mike@updroid.com>
2015-09-12 17:52:40 -0700

    tool: fixed hardcoded tab paths

--- tool/build_cmdr.sh
+++ tool/build_cmdr.sh
@@ -102,19 +102,18 @@ WEBPANELS=$BUILD/web/panels
 mkdir -p $BINPANELS
 mkdir -p $WEBPANELS
 
-# TODO fix these hard-coded paths
-cp -r /home/crunchex/work/upcom-explorer/build/bin $BINPANELS/upcom-explorer
-cp -r /home/crunchex/work/upcom-editor/build/bin $BINTABS/upcom-editor
-cp -r /home/crunchex/work/upcom-console/build/bin $BINTABS/upcom-console
-# cp -r /home/crunchex/work/upcom-camera/build/bin $BINTABS/upcom-camera
-# cp -r /home/crunchex/work/upcom-shop/build/bin $BINTABS/upcom-shop
+cp -r ../upcom-explorer/build/bin $BINPANELS/upcom-explorer
+cp -r ../upcom-editor/build/bin $BINTABS/upcom-editor
+cp -r ../upcom-console/build/bin $BINTABS/upcom-console
+# cp -r ../upcom-camera/build/bin $BINTABS/upcom-camera
+# cp -r ../upcom-shop/build/bin $BINTABS/upcom-shop
 # cp -r ../upcom-teleop/build/bin $BINTABS/upcom-teleop
 # cp -r ../upcom-learn-demo/build/bin $BINTABS/upcom-learn-demo
 
-cp -r /home/crunchex/work/upcom-explorer/build/web $WEBPANELS/upcom-explorer
-cp -r /home/crunchex/work/upcom-editor/build/web $WEBTABS/upcom-editor
-cp -r /home/crunchex/work/upcom-console/build/web $WEBTABS/upcom-console
-# cp -r /home/crunchex/work/upcom-camera/build/web $WEBTABS/upcom-camera
+cp -r ../upcom-explorer/build/web $WEBPANELS/upcom-explorer
+cp -r ../upcom-editor/build/web $WEBTABS/upcom-editor
+cp -r ../upcom-console/build/web $WEBTABS/upcom-console
+# cp -r ../upcom-camera/build/web $WEBTABS/upcom-camera
 # cp -r ../upcom-teleop/build/web $WEBTABS/upcom-teleop
 # cp -r ../upcom-learn-demo/build/web $WEBTABS/upcom-learn-demo
 echo "OK"

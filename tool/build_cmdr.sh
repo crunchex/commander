a2f57d3d55575ad9c5f06c5bb0c77eeadf08e5fc
Mike Lewis <mike@updroid.com>
2015-10-11 10:12:36 -0400

    tool: fixed os x bugs in build script

--- tool/build_cmdr.sh
+++ tool/build_cmdr.sh
@@ -131,6 +131,10 @@ if [ $(uname -s) != "Darwin" ]; then
     sed -i 's/main.css/cmdr-min.css/g' $BUILD/index.html
     sed -i 's/main.dart/main.dart.js/g' $BUILD/index.html
 fi
+if [ $(uname -s) == "Darwin" ]; then
+    sed -i '' 's/main.css/cmdr-min.css/' $BUILD/index.html
+    sed -i '' 's/main.dart/main.dart.js/' $BUILD/index.html
+fi
 echo "OK"
 
 ### cmdr ###
@@ -148,6 +152,9 @@ fi
 rm -rf $BINDIR/cmdr.deps
 if [ $(uname -s) != "Darwin" ]; then
     sed -i '1i#!/usr/bin/env dart' $BINDIR/cmdr
+fi
+if [ $(uname -s) == "Darwin" ]; then
+    perl -pi -e 'print "#!/usr/bin/env dart\n" if $. == 1' $BINDIR/cmdr
 fi
 chmod +x $BINDIR/cmdr
 echo "OK"

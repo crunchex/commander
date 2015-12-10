01009ed1366a0c4a820ea8bf1f4b8343685a1942
Mike Lewis <mike@updroid.com>
2015-12-10 12:52:20 -0800

    cmdr: added comment to help find the meaningful entry point

--- bin/cmdr.dart
+++ bin/cmdr.dart
@@ -7,6 +7,7 @@ import '../lib/server.dart';
 void main(List<String> args) {
   const String version = '0.5.3 Alpha';
 
+  // Entry point for [CmdrServer] can be found in commands.dart under [CmdrGuiCommand].
   CommandRunner runner = new CommandRunner("cmdr", "UpDroid Commander - Robotics Software IDE.")
     ..addCommand(new InfoCommand(version))
     ..addCommand(new BuildLogCommand())
